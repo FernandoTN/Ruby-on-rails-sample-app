26ce54aed38a8c021730395b46b72cde06418f2b
Fernando <fertorresnavarrete@hotmail.com>
2014-10-05 15:05:19 -0500

    fixed db:migrate

--- db/migrate/20141001220747_create_users.rb
+++ db/migrate/20141001220747_create_users.rb
@@ -1,10 +1,9 @@
-bundclass CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
       t.string :email
-
       t.timestamps
     end
   end
-end
+ end
