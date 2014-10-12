4992f88b72b422ecfb259e2cb2c8c1eeadd1b2db
Fernando <fertorresnavarrete@hotmail.com>
2014-10-12 13:36:11 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,44 @@
 class UsersController < ApplicationController
+  before_action :restrict_registration, only: [:new, :create]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+     @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
   def new
-  end
-
-   def new
     @user = User.new
   end
 
- def create
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -24,8 +51,30 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
+  def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+  # Before filters
+
+   def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+     def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+    def restrict_registration
+       redirect_to root_url, notice: "You are already regsitered." if signed_in?
     end
 end
